81080c1a6a8868d73df39aad781c7b776768a957
blablabla <blablabla@gmail.com>
2025-02-25 16:46:51 +0200

    dodano translate

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,13 +1,16 @@
-public class Point {
-    public double x;
-    public double y;
-}
-
 public class Main {
     public static void main(String[] args) {
         Point p1 = new Point();
-        p1.x = 5.0;
-        p1.y = 4.0;
-        System.out.println("P1: ("+p1.x+","+p1.y+")");
+        p1.x = 30.0;
+        p1.y = 40.0;
+
+        System.out.println(p1);
+        System.out.println(p1.toSvg());
+
+        p1.translate(-6, 5);
+        System.out.println(p1);
+        Point tr = p1.translated(6,-5);
+        System.out.println(tr);
+        System.out.println(p1.translated(6,-5));
     }
 }
--- svg/src/Point.java
+++ svg/src/Point.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    public double x;
+    public double y;
+
+    @Override
+    public String toString(){
+        return "("+x+","+y+")";
+    }
+
+    public String toSvg(){
+        return "<circle r=\""+x+"\" cx=\""+y+"\" cy=\"50\" fill=\"red\" />";
+    }
+
+    public void translate(double dx, double dy){
+        x += dx;
+        y += dy;
+    }
+
+    public Point translated(double dx, double dy){
+        Point newPoint = new Point();
+        newPoint.x = x+dx;
+        newPoint.y = y+dy;
+        return newPoint;
+    }
 }
